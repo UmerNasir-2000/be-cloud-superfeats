2dc3a526fb14201a04c36fbc310dc73c8efd9013
Muhammad Umer Nasir <67263090+UmerNasir-2000@users.noreply.github.com>
2023-09-29 22:17:01 +0500

    [FEAT] Added CORS 🔐

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import AppModule from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({ origin: '*' });
+
   const config = new DocumentBuilder()
     .setTitle('Superfeats API')
     .setDescription('REST API docs for Superfeats')
